c06c7052b88688b245212051be4b0863f2db6752
jcfs <joao.salavisa@gmail.com>
2016-01-23 17:36:19 +0000

    pid timer pid tweaks new constants file

--- src/pid_motor_speed.cpp
+++ src/pid_motor_speed.cpp
@@ -5,21 +5,26 @@
 #include "PID_v1.h"
 #include "printf.h"
 #include "utils.h"
+#include "constants.h"
 
 double pid_output_accel, pid_setpoint_angle, pid_input_angle;
 
 // pid to obtain the desired speed given the current angle and the desired angle
-PID pid_controller_speed(&pid_input_angle, &pid_output_accel, &pid_setpoint_angle, 1.5, 0, 0.8, DIRECT);
+PID pid_controller_speed(&pid_input_angle, &pid_output_accel, &pid_setpoint_angle, KP, KI, KD, DIRECT);
 
 void setup_pid() {
   pid_controller_speed.SetMode(AUTOMATIC);
-  pid_controller_speed.SetOutputLimits(-20, 20);
+  pid_controller_speed.SetOutputLimits(-MAX_ACCEL, MAX_ACCEL);
+  pid_controller_speed.SetSampleTime(10); // 10ms
 }
+
+
 
 //  * Get the current angle from the MPU
 //  * Feed the current angle to the speed PID to obtain the desired acceleration to get to the desired angle
 //  * Adjust motor speeds accordingly
 void get_pid_motor_speed(int16_t * motor_accel, float angle, float angle_old, int16_t m1, int16_t m2) {
+
   pid_setpoint_angle = 2; // for now we'll ignore the first pid computation
   pid_input_angle = angle;
   pid_controller_speed.Compute();
