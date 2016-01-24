5b9d1ef7793fa632db99aa0a8c9bfdeb84257cef
jcfs <joao.salavisa@gmail.com>
2016-01-24 01:11:43 +0000

    constants

---
+++ src/constants.h
@@ -0,0 +1,19 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define KP  1.5
+#define KI  0
+#define KD  0.8
+
+#define MAX_ACCEL 50
+
+#define P1_X 500
+#define P1_Y 2
+
+#define P2_X 0
+#define P2_Y 30
+
+#endif
+
+
+
